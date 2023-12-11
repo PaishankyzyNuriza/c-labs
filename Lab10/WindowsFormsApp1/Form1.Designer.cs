38b364bcd0bedf6f9a8cb5cdb16bf6fb5ed8f0a1
Nuriza Paishan kyzy <73305001+PaishankyzyNuriza@users.noreply.github.com>
2023-12-11 15:17:50 +0600

    Add files via upload

---
+++ Lab10/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.перваяФормаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.втораяФормаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.стилиОтображенияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.arrangeItemsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cascadeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tileHorizontalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tileVerticalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.расчетToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.перваяФормаToolStripMenuItem,
+            this.втораяФормаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // перваяФормаToolStripMenuItem
+            // 
+            this.перваяФормаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.стилиОтображенияToolStripMenuItem});
+            this.перваяФормаToolStripMenuItem.Name = "перваяФормаToolStripMenuItem";
+            this.перваяФормаToolStripMenuItem.Size = new System.Drawing.Size(100, 20);
+            this.перваяФормаToolStripMenuItem.Text = "Первая форма";
+            // 
+            // втораяФормаToolStripMenuItem
+            // 
+            this.втораяФормаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.расчетToolStripMenuItem});
+            this.втораяФормаToolStripMenuItem.Name = "втораяФормаToolStripMenuItem";
+            this.втораяФормаToolStripMenuItem.Size = new System.Drawing.Size(98, 20);
+            this.втораяФормаToolStripMenuItem.Text = "Вторая Форма";
+            // 
+            // стилиОтображенияToolStripMenuItem
+            // 
+            this.стилиОтображенияToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arrangeItemsToolStripMenuItem,
+            this.cascadeToolStripMenuItem,
+            this.tileHorizontalToolStripMenuItem,
+            this.tileVerticalToolStripMenuItem});
+            this.стилиОтображенияToolStripMenuItem.Name = "стилиОтображенияToolStripMenuItem";
+            this.стилиОтображенияToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.стилиОтображенияToolStripMenuItem.Text = "Стили отображения";
+            // 
+            // arrangeItemsToolStripMenuItem
+            // 
+            this.arrangeItemsToolStripMenuItem.Name = "arrangeItemsToolStripMenuItem";
+            this.arrangeItemsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.arrangeItemsToolStripMenuItem.Text = "ArrangeItems";
+            this.arrangeItemsToolStripMenuItem.Click += new System.EventHandler(this.arrangeItemsToolStripMenuItem_Click);
+            // 
+            // cascadeToolStripMenuItem
+            // 
+            this.cascadeToolStripMenuItem.Name = "cascadeToolStripMenuItem";
+            this.cascadeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cascadeToolStripMenuItem.Text = "Cascade";
+            this.cascadeToolStripMenuItem.Click += new System.EventHandler(this.cascadeToolStripMenuItem_Click);
+            // 
+            // tileHorizontalToolStripMenuItem
+            // 
+            this.tileHorizontalToolStripMenuItem.Name = "tileHorizontalToolStripMenuItem";
+            this.tileHorizontalToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tileHorizontalToolStripMenuItem.Text = "TileHorizontal";
+            this.tileHorizontalToolStripMenuItem.Click += new System.EventHandler(this.tileHorizontalToolStripMenuItem_Click);
+            // 
+            // tileVerticalToolStripMenuItem
+            // 
+            this.tileVerticalToolStripMenuItem.Name = "tileVerticalToolStripMenuItem";
+            this.tileVerticalToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.tileVerticalToolStripMenuItem.Text = "TileVertical";
+            this.tileVerticalToolStripMenuItem.Click += new System.EventHandler(this.tileVerticalToolStripMenuItem_Click);
+            // 
+            // расчетToolStripMenuItem
+            // 
+            this.расчетToolStripMenuItem.Name = "расчетToolStripMenuItem";
+            this.расчетToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.расчетToolStripMenuItem.Text = "Расчет";
+            this.расчетToolStripMenuItem.Click += new System.EventHandler(this.расчетToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem перваяФормаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem стилиОтображенияToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem arrangeItemsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cascadeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tileHorizontalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tileVerticalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem втораяФормаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem расчетToolStripMenuItem;
+    }
+}
+
---
+++ Lab10/WindowsFormsApp1/Form3.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form3 : Form
+    {
+        public int[] mas = new int[4] { 30, 34, 195, 200 };
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ListViewItem newItem = listView1.Items.Add(comboBox1.SelectedItem.ToString());
+            newItem.SubItems.Add(textBox1.Text);
+            newItem.SubItems.Add(textBox2.Text);
+            newItem.SubItems.Add(textBox3.Text);
+            double r = 0;
+            // loop through the entire list and summarize the 4th column
+            foreach (ListViewItem lvi in listView1.Items)
+            {
+                r = Convert.ToDouble(lvi.SubItems[3].Text) + r;
+            }
+            textBox4.Text = r.ToString();
+
+            Clear();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = mas[comboBox1.SelectedIndex].ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "")
+                textBox3.Text = Convert.ToString(Convert.ToDouble(textBox1.Text) * Convert.ToDouble(textBox2.Text.Replace(".", ",")));
+
+        }
+
+        void Clear()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+    }
+}
